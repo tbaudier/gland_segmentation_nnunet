2c0314fefc805cde8746a907319498ed421a3e73
LimuleSempai <benoit.catez@epita.fr>
2024-05-14 16:12:04 +0200

    fix definecrop.py

--- Data_preparation/definecrop.py
+++ Data_preparation/definecrop.py
@@ -6,8 +6,8 @@ def find_crop_limits(skull_path = str , patients = dict):
     Find the highest and lowest limit to crop for the entire dataset.
 
     Args:
-        skull_path (str): enter the path where the skull are stored. (e.g '/home/bcatez/data/DatasetSkull_glands/skull/')
-        patients (disct): enter the patients list ( a dict found in the patient.json file)
+        skull_path (str): path where the skull are stored. (e.g '/home/bcatez/data/DatasetSkull_glands/skull/')
+        patients (dict): patients list ( a dict found in the patient.json file)
 
     Returns:
         int: the biggest skull size + 100.
