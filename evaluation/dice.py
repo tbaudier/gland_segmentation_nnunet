b0a7860ac71c7b2a605efb15c59215755206f726
LimuleSempai <benoit.catez@epita.fr>
2024-06-06 15:02:59 +0200

    update dice.py

--- evaluation/dice.py
+++ evaluation/dice.py
@@ -9,11 +9,11 @@ def dice(inf, label):
     label = itk.GetArrayFromImage(itk.imread(label))
 
 
-    for val in range(1,7):
+    for val in range(6):
         inf_ = inf.copy()
         label_ = label.copy()
-        inf_[inf_ != val] = 0
-        label_[label_ != val] = 0
+        inf_[inf_ != (val+1)] = 0
+        label_[label_ != (val+1)] = 0
         inf_bool = np.asarray(inf_).astype(np.bool_)
         label_bool = np.asarray(label_).astype(np.bool_)
 
@@ -26,3 +26,15 @@ def dice(inf, label):
         result.append(2.*intersection.sum()/(inf_bool.sum() + label_bool.sum()))
 
     return result
+
+
+def dice_result(imagesTs :list):
+    dice_results = np.zeros((5,6))
+
+    for i in range(len(imagesTs)):
+        dice_results[i] = dice(inf="/home/bcatez/data/nnUNet_raw/Dataset004_glands/imagesTs_pred_/p0" + str(imagesTs[i]) + "_psma.nii.gz",
+                            label="/home/bcatez/data/nnUNet_raw/Dataset004_glands/labelsTr/p0" + str(imagesTs[i]) + "_psma.nii.gz")
+
+    np.save("/home/bcatez/data/nnUNet_raw/Dataset004_glands/imagesTs_pred_/dice_results.npy",dice_results)
+
+dice_result([12,16,21,22,47])
