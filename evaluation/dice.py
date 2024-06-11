88cc608b51baef116140b61fe8db3455d49c1697
LimuleSempai <benoit.catez@epita.fr>
2024-06-11 10:57:45 +0200

    better variable names

--- evaluation/dice.py
+++ evaluation/dice.py
@@ -2,29 +2,29 @@ import itk
 import numpy as np
 import json
 
-def dice(inf, label):
+def dice(inference, label):
 
     result = []
     
-    inf = itk.GetArrayFromImage(itk.imread(inf))
-    label = itk.GetArrayFromImage(itk.imread(label))
+    inf = itk.GetArrayFromImage(itk.imread(inference))
+    lab = itk.GetArrayFromImage(itk.imread(label))
 
 
     for val in range(6):
         inf_ = inf.copy()
-        label_ = label.copy()
+        lab_ = lab.copy()
         inf_[inf_ != (val+1)] = 0
-        label_[label_ != (val+1)] = 0
+        lab_[lab_ != (val+1)] = 0
         inf_bool = np.asarray(inf_).astype(np.bool_)
-        label_bool = np.asarray(label_).astype(np.bool_)
+        lab_bool = np.asarray(lab_).astype(np.bool_)
 
-        if inf_bool.shape != label_bool.shape:
+        if inf_bool.shape != lab_bool.shape:
             raise ValueError("Shape mismatch: inference and label must have the same shape.")
     
     # Compute Dice coefficient
-        intersection = np.logical_and(inf_bool,label_bool)
+        intersection = np.logical_and(inf_bool,lab_bool)
 
-        result.append(2.*intersection.sum()/(inf_bool.sum() + label_bool.sum()))
+        result.append(2.*intersection.sum()/(inf_bool.sum() + lab_bool.sum()))
 
     return result
 
@@ -33,8 +33,8 @@ def dice_result(imagesTs :list[int], dataset_name : str):
     dice_results = np.zeros((len(imagesTs),6))
 
     for i in range(len(imagesTs)):
-        dice_results[i] = dice(inf="/home/bcatez/data/nnUNet_raw/" + dataset_name + "/imagesTs_pred_/p0" + str(imagesTs[i]) + "_psma.nii.gz",
-                            label="/home/bcatez/data/nnUNet_raw/" + dataset_name + "/labelsTr/p0" + str(imagesTs[i]) + "_psma.nii.gz")
+        dice_results[i] = dice(inference="/home/bcatez/data/idris/nnUNet_raw/" + dataset_name + "/imagesTs_pred_/p0" + str(imagesTs[i]) + "_psma.nii.gz",
+                            label="/home/bcatez/data/idris/nnUNet_raw/" + dataset_name + "/labelsTr/p0" + str(imagesTs[i]) + "_psma.nii.gz")
 
     file = dice_results.tolist()
     with open('/home/bcatez/data/nnUNet_raw/' + dataset_name + '/imagesTs_pred_/dice_results.json','w', encoding='utf-8') as f:
